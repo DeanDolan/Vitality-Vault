9eb785fb5635b9831bb2506148792560e21e04f6
DeanDolan <115548512+DeanDolan@users.noreply.github.com>
2024-10-23 16:14:22 +0100

    Add files via upload

---
+++ Vitality Vault/src/vitalityvault/VVH.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vitalityvault;
+
+/**
+ *
+ * @author conor
+ */
+public class VVH {
+    int age, gWeight, cWeight,mKcal,cKcal,gKcal,activity, timeframe;
+    String goals, comments;
+    
+    
+    public int getmKcal() {
+        return mKcal;
+    }
+
+
+    public int getcKcal() {
+        return cKcal;
+    }
+
+
+    public int getgKcal() {
+        return gKcal;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getgWeight() {
+        return gWeight;
+    }
+
+    public void setgWeight(int gWeight) {
+        this.gWeight = gWeight;
+    }
+
+    public int getcWeight() {
+        return cWeight;
+    }
+    
+    public void setcWeight(int cWeight) {
+        this.cWeight = cWeight;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+
+    public void setGoals(String goals) {
+        this.goals = goals;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public int getActivity() {
+        return activity;
+    }
+
+    public void setActivity(int activity) {
+        this.activity = activity;
+    }
+
+    public int getTimeframe() {
+        return timeframe;
+    }
+
+    public void setTimeframe(int timeframe) {
+        this.timeframe = timeframe;
+    }
+    
+}
